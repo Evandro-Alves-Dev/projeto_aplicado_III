e128b67b6e646d5b54adf69416d27e60b6fdcc09
Evandro Alves <evandroacer3@gmail.com>
2024-06-18 08:35:01 -0300

    config: Adicionado configurações do projeto

---
+++ src/main/java/com/senai/pa3/config/CustomLocalDateDeserializer.java
@@ -0,0 +1,18 @@
+package com.senai.pa3.config;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class CustomLocalDateDeserializer extends JsonDeserializer<LocalDate> {
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    @Override
+    public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        return LocalDate.parse(p.getValueAsString(), formatter);
+    }
+}
